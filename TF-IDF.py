67a2f9fc7f10fc6a1171da4b6aa04e6b723c0a04
Mr2316 <yqbp2316@163.com>
2021-04-01 08:48:00 +0800

    try tf-idf

--- TF-IDF.py
+++ TF-IDF.py
@@ -2,64 +2,53 @@ import numpy as np
 from collections import Counter
 import itertools
 
-#from visual import show_tfidf
+np.set_printoptions(threshold=np.inf)
 
 docs = [
-    "it is a good day, I like to stay here",
-    "I am happy to be here",
-    "I am bob",
-    "it is sunny today",
-    "I have a party today",
-    "it is a dog and that is a cat",
-    "there are dog and cat on the tree",
-    "I study hard this morning",
-    "today is a good day",
-    "tomorrow will be a good day",
-    "I like coffee, I like book and I like apple",
-    "I do not like it",
-    "I am kitty, I like bob",
-    "I do not care who like bob, but I like kitty",
-    "It is coffee time, bring your cup",
+	"Why so serious",
+	"Because I am batman",
+	"Things always get worse before they get better",
+	"Let us put a smile on that face",
+	"Now I see the funny side,now I am always smiling",
+	"Sometimes people deserve to have their faith rewarded",
+	"I am whatever Gotham needs me to be",
+	"I am the chaos",
+	"it is not who I am underneath,but what I do that defines me",
+	"You either die a hero or you live long enough to see yourself become the villain",
+	"I just wanna see her simle again",
+	"You know,madness is a lot like gravity,sometimes all you need is a little push",
+	"I belive,whatever does not kill you simply makes you stranger"
 ]
 
-docs_words = [d.replace(",","").split(" ")for d in docs]	#分词
-vocab = set(itertools.chain(*docs_words))					#去重
-vandi = {v:i for i,v in enumerate(vocab)}					#编号
-iandv = {i:v for v,i in vandi.items()}
+words_in_docs = [d.replace(","," ").split(" ") for d in docs]									#分词    [["Why","so","serious"]]
 
-tf_methods = {
-	"log": lambda x :np.log(1+x),
-	"augmented": lambda x: 0.5 + 0.5 * x / np.max(x, axis=1, keepdims=True),
-	"boolean": lambda x: np.minimum(x, 1),
-	"log_avg": lambda x: (1 + safe_log(x)) / (1 + safe_log(np.mean(x, axis=1, keepdims=True))),
-}
+allwords = set(itertools.chain(*words_in_docs))													#拆分并去重   ["Why","so"]
 
-idf_methods = {
-	"log":lambda x:1+np.log(len(docs)/(x+1)),
-	"prob":lambda x:np.maximum(0, np.log((len(docs) - x) / (x+1))),
-	"len-norm":lambda x:x/(np.sum(np.square(x)+1)),
-}
+vtoi = {v:i for i,v in enumerate(allwords)}														#{词:序号}
+itov = {i:v for v,i in vtoi.items()}															#{序号：词}
+
+def get_tf():
+	_tf = np.zeros((len(allwords),len(docs)),dtype = np.float64)								#shape = (词数,文章数)
+	for i,d in enumerate(words_in_docs):
+		counter = Counter(d)
+		for v in counter.keys():
+			_tf[vtoi[v],i] = counter[v] / counter.most_common(1)[0][1]
+
+	return np.log(1+_tf)
 
 
-def get_tf(method = "log"):
-	_tf = np.zeros((len(vocab),len(docs)),dtype = np.float64)
-	for i,d in enumerate(docs_words):
-		print(i)
-		print(d)
+def get_idf():
+	df = np.zeros((len(allwords),1))															#shape = (词数,1)
+	for i in range(len(allwords)):
+		dcount = 0																				#词频计数器
+		for d in words_in_docs:
+			dcount += 1 if itov[i] in d else 0
+		df[i,0] = dcount
 
+	return 1+np.log(len(docs)/(df + 1))
 
-def get_idf(method = "log"):
-	df = np.zeros((len(iandv),1))
-	for i in range(len(iandv)):
-		d_count = 0
-		for d in docs_words:
-			d_count += 1 if iandv[i] in d else 0
-		df[i, 0] = d_count
+tf = get_tf()
+idf = get_idf()
+tf_idf = tf*idf
 
-
-	idf_fn = idf_methods.get(method,None)
-	if idf_fn is None:
-		raise ValueError
-	return idf_fn(df)
-
-get_tf()
+np.savetxt("tf_idf.txt",tf_idf,fmt="%f", delimiter=",")
