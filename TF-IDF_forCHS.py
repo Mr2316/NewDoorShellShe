67f8b0d77d1f6553253ba409143a2cbaaa805c97
Mr2316 <yqbp2316@163.com>
2021-04-10 15:34:49 +0800

    20210410

---
+++ TF-IDF_forCHS.py
@@ -0,0 +1,12 @@
+import numpy as np 
+from collections import Counter
+import itertools
+
+
+np.set_printoptions(threshold=np.inf)
+np.seterr(divide='ignore',invalid='ignore')
+
+docs = [
+	"君不见 黄河之水天上来 奔流到海不复回",
+	
+]
